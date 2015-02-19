695c283ab21db5aac538166c048f567cf39b910d
wataruoguchi <wataru.oguchi.0to1@gmail.com>
2015-02-18 18:36:14 -0800

    Optimization for mobile
    Add media query for mobile, move 2 css file links to bottom

--- js/controllers.js
+++ js/controllers.js
@@ -5,7 +5,7 @@
 	app.controller('WorksController', function($scope, $http, $sce){
 		//http://www.9lessons.info/2013/08/angularjs-tutorial-restful.html
 		//Add data resource
-		$http({method: 'GET', url:'./asset/works.json'}).success(function(response)
+		$http({cache: true, method: 'GET', url:'./asset/works.json'}).success(function(response)
 		{
 			$.map(response.works, function(item) {
 				item.description = $sce.trustAsHtml(item.description);
@@ -19,7 +19,7 @@
 	app.controller('QualitiesController', function($scope, $http){
 		//http://www.9lessons.info/2013/08/angularjs-tutorial-restful.html
 		//Add data resource
-		$http({method: 'GET', url:'./asset/qualities.json'}).success(function(response)
+		$http({cache: true, method: 'GET', url:'./asset/qualities.json'}).success(function(response)
 		{
 			$scope.results = response.qualities;
 		});
@@ -28,7 +28,7 @@
 	//Skills
 	app.controller('SkillsController', function($scope, $http){
 		//Add data resource
-		$http({method: 'GET', url:'./asset/skills.json'}).success(function(response)
+		$http({cache: true, method: 'GET', url:'./asset/skills.json'}).success(function(response)
 		{
 			$scope.summaries = response.summaries;
 			$scope.tools = response.tools;
